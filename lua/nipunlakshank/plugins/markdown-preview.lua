9be0d904c832f03d70e59daf5645676c222b7e58
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-05-02 03:39:13 +0530

    Fixed a issue with markdown-preview build when upgrading

--- lua/nipunlakshank/plugins/markdown-preview.lua
+++ lua/nipunlakshank/plugins/markdown-preview.lua
@@ -1,7 +1,7 @@
 return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install",
+    build = "cd app && npx --yes yarn install",
     init = function()
         vim.g.mkdp_filetypes = { "markdown" }
     end,
