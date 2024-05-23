b81e1fcfeb6144182f90aba7319ad15421c1374d
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-05-23 10:46:11 +0530

    Enabled numToStr/Comment.nvim again
    * previously disabled if neovim is 0.10.0 or later
    * It was replaced by ts-comments.nvim but it wasn't a replacement to
      Comment.nvim but a enhancement to native comments

--- lua/nipunlakshank/plugins/comment.lua
+++ lua/nipunlakshank/plugins/comment.lua
@@ -2,5 +2,6 @@ return {
     "numToStr/Comment.nvim",
     event = { "BufReadPre", "BufNewFile" },
     config = true,
-    enabled = vim.fn.has("nvim-0.10.0") == 0,
+    -- NOTE: this was enabled again due to ts-comments not replacing the same functionality but enhancing native comments
+    -- enabled = vim.fn.has("nvim-0.10.0") == 0,
 }
