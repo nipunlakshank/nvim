bac790e9d75d92925f8cc6f3c604e6962dd90ad5
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-05-23 10:55:10 +0530

    Add pre_hook to setup
    * This is for integrating nvim-ts-commentstring plugin

--- lua/nipunlakshank/plugins/comment.lua
+++ lua/nipunlakshank/plugins/comment.lua
@@ -1,7 +1,11 @@
 return {
-    "numToStr/Comment.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    config = true,
-    -- NOTE: this was enabled again due to ts-comments not replacing the same functionality but enhancing native comments
-    -- enabled = vim.fn.has("nvim-0.10.0") == 0,
+	"numToStr/Comment.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("Comment").setup({
+			pre_hook = function()
+				return vim.bo.commentstring
+			end,
+		})
+	end,
 }
