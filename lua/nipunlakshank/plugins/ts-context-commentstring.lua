bec39150f6eb620ba2c15006f408e8d24d03ebc9
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-05-23 11:08:30 +0530

    feat: integrated nvim-ts-context-commentstring
    * Deleted ts-context-commentstring.lua file
    * Add nvim-ts-context-commentstring as a dependency to Comment.nvim
    * Fixed correct Comment.nvim pre_hook

--- lua/nipunlakshank/plugins/ts-context-commentstring.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "JoosepAlviste/nvim-ts-context-commentstring",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        require("ts_context_commentstring").setup({})
-    end,
-}
