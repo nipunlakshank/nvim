02ed1dc9eac35304fff9ef0626fbdebfe532d6c8
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-05-25 11:35:50 +0530

    Changed lualine style

--- lua/nipunlakshank/plugins/lualine.lua
+++ lua/nipunlakshank/plugins/lualine.lua
@@ -1,24 +1,37 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    event = { "VeryLazy" },
-    config = function()
-        local lualine = require("lualine")
-        lualine.setup({
-            options = {
-                theme = "auto",
-                globalstatus = true,
-                component_separators = { left = "|", right = "|" },
-                section_separators = { left = "", right = "" },
-            },
-            sections = {
-                lualine_a = { "mode" },
-                lualine_b = { "buffers" },
-                lualine_x = { "diff", "branch" },
-                lualine_y = { "encoding", "fileformat", "filetype" },
-                lualine_z = { "progress", "location" },
-            },
-            tabline = {},
-        })
-    end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"tpope/vim-fugitive",
+	},
+	event = { "VeryLazy" },
+	config = function()
+		local lualine = require("lualine")
+
+		lualine.setup({
+			options = {
+				theme = auto,
+				global_status = true,
+				component_separators = "",
+				section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "filename", "diff" },
+				lualine_c = { "%=", "buffers" },
+				lualine_x = { { "FugitiveHead", icon = { "", color = { fg = "Orange" } } } },
+				lualine_y = { "encoding", "fileformat", "filetype" },
+				lualine_z = { "progress", "location" },
+			},
+			inactive_sections = {
+				lualine_a = { "filename" },
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = { "location" },
+			},
+			tabline = {},
+		})
+	end,
 }
