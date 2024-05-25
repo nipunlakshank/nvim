29e2d03497c7efcb8b5e4eab9d41f1806cdc8436
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-05-25 11:34:07 +0530

    Code formatted

--- lua/nipunlakshank/config/options.lua
+++ lua/nipunlakshank/config/options.lua
@@ -53,7 +53,7 @@ opt.cmdheight = 1
 opt.scrolloff = 10
 opt.fillchars = { eob = " " }
 opt.guicursor =
-"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+	"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 
 -- folds
 opt.foldmethod = "expr"
