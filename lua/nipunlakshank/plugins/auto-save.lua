190947a94a18e3d6ec83b2d37f2201b8a7b800ee
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-06-07 21:15:30 +0530

    Add callbacks to auto-save plugin

--- lua/nipunlakshank/plugins/auto-save.lua
+++ lua/nipunlakshank/plugins/auto-save.lua
@@ -6,6 +6,20 @@ return {
         autosave.setup({
             dim = 0.18,
             cleaning_interval = 1000,
+            callbacks = { -- functions to be executed at different intervals
+                enabling = nil, -- ran when enabling auto-save
+                disabling = nil, -- ran when disabling auto-save
+
+                -- FIX: this is not working
+                before_asserting_save = function()
+                    if vim.bo.filetype == "harpoon" then
+                        return false
+                    end
+                end, -- ran before checking `condition`
+
+                before_saving = nil, -- ran before doing the actual save
+                after_saving = nil, -- ran after doing the actual save
+            },
         })
         autosave.toggle()
     end,
