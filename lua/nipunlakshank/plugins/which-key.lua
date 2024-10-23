822104e95191d0f97d7dd0eedd962e942e0219bb
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-10-23 15:26:21 +0530

    Config updated to work with latest version

--- lua/nipunlakshank/plugins/which-key.lua
+++ lua/nipunlakshank/plugins/which-key.lua
@@ -1,17 +1,18 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 300
-	end,
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-		popup_mappings = {
-			scroll_down = "<C-j>", -- binding to scroll down inside the popup
-			scroll_up = "<C-k>", -- binding to scroll up inside the popup
-		},
-	},
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+    end,
+    opts = {},
+    keys = {
+        {
+            "<leader>?",
+            function()
+                require("which-key").show({ global = false })
+            end,
+            desc = "Buffer Local Keymaps (which-key)",
+        },
+    },
 }
