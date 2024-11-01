45cd97f6d61ccb7ab0e0010bea0e98ab2d38483b
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-11-01 23:38:07 +0530

    Changed from nvim_rocks to luarocks

--- lua/nipunlakshank/config/paths.lua
+++ lua/nipunlakshank/config/paths.lua
@@ -6,5 +6,5 @@
 --     package.path = package.path .. ";" .. "C:\\ProgramData\\chocolatey\\lib\\luarocks\\luarocks-2.4.4-win32\\lua\\?.lua;"
 --     package.path = package.path .. ";" .. "C:\\ProgramData\\chocolatey\\lib\\luarocks\\luarocks-2.4.4-win32\\lua\\?\\init.lua;"
 -- end
--- package.path = package.path .. ";" .. vim.fn.expand("$XDG_CONFIG_HOME") .. "/luarocks/share/lua/5.1/?/init.lua;"
--- package.path = package.path .. ";" .. vim.fn.expand("$XDG_CONFIG_HOME") .. "/luarocks/share/lua/5.1/?.lua;"
+package.path = package.path .. ";" .. vim.fn.expand("~/.luarocks/share/lua/5.1/?/init.lua;")
+package.path = package.path .. ";" .. vim.fn.expand("~/.luarocks/share/lua/5.1/?.lua;")
