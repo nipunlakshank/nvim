cf8bd3d3efba0ef7125dffa39d248ddf4b7556ab
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-11-06 07:34:25 +0530

    Migrated from neodev to lazydev
     - this improved ram usage by lua_ls considerably

--- lua/nipunlakshank/plugins/init.lua
+++ lua/nipunlakshank/plugins/init.lua
@@ -1,33 +1,53 @@
--- if vim.fn.has("nvim-0.10.0") == 1 then
-if false then
-    return {
-        {
-            "folke/lazydev.nvim",
-            ft = "lua", -- only load on lua files
-            dependencies = {
-                "folke/neoconf.nvim",
-                { "Bilal2453/luvit-meta" }, -- optional `vim.uv` typings
+return {
+    {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+            library = {
+                -- See the configuration section for more details
+                -- Load luvit types when the `vim.uv` word is found
+                { path = "luvit-meta/library", words = { "vim%.uv" } },
+                { path = "LazyVim", words = { "LazyVim" } },
+                { path = "wezterm-types", mods = { "wezterm" } },
             },
-            opts = {
-                library = {
-                    vim.env.LAZY .. "/luvit-meta/library",
+            -- always enable unless `vim.g.lazydev_enabled = false`
+            -- This is the default
+            enabled = function(root_dir)
+                return vim.g.lazydev_enabled == nil and true or vim.g.lazydev_enabled
+            end,
+
+            -- -- disable when a .luarc.json file is found
+            -- enabled = function(root_dir)
+            --     return not vim.uv.fs_stat(root_dir .. "/.luarc.json")
+            -- end,
+        },
+    },
+    { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
+    { -- optional cmp completion source for require statements and module annotations
+        "hrsh7th/nvim-cmp",
+        opts = function(_, opts)
+            opts.sources = opts.sources or {}
+            table.insert(opts.sources, {
+                name = "lazydev",
+                group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+            })
+        end,
+    },
+    { -- optional blink completion source for require statements and module annotations
+        "saghen/blink.cmp",
+        opts = {
+            sources = {
+                -- add lazydev to your completion providers
+                completion = {
+                    enabled_providers = { "lsp", "path", "snippets", "buffer", "lazydev" },
+                },
+                providers = {
+                    -- dont show LuaLS require statements when lazydev has items
+                    lsp = { fallback_for = { "lazydev" } },
+                    lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
                 },
             },
         },
-        { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
-    }
-else
-    return {
-        {
-            "folke/neodev.nvim",
-            dependencies = {
-                "folke/neoconf.nvim",
-            },
-            config = function()
-                require("neoconf").setup({})
-                require("neodev").setup({})
-            end,
-        },
-        { "folke/lazydev.nvim", enabled = false },
-    }
-end
+    },
+    -- { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
+}
