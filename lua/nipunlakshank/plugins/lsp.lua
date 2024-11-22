7f0778edf2a3a7176e637ab53f929b2299369de4
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-11-22 22:05:44 +0530

    chore(formatting):replace none-ls with conform

--- lua/nipunlakshank/plugins/lsp.lua
+++ lua/nipunlakshank/plugins/lsp.lua
@@ -215,38 +215,4 @@ return {
             })
         end,
     },
-    {
-        "williamboman/mason.nvim",
-        cmd = { "Mason", "MasonInstall", "MasonUninstall", "MasonUninstallAll", "MasonUpdate", "MasonLog" },
-        dependencies = {
-            "jay-babu/mason-null-ls.nvim",
-            "nvimtools/none-ls.nvim",
-        },
-        config = function()
-            local mason = require("mason")
-            local mason_null_ls = require("mason-null-ls")
-
-            mason.setup({
-                ui = {
-                    border = "rounded",
-                    icons = {
-                        package_installed = "✓",
-                        package_pending = "➜",
-                        package_uninstalled = "✗",
-                    },
-                },
-                registries = {
-                    "github:nvim-java/mason-registry",
-                    "github:mason-org/mason-registry",
-                },
-            })
-
-            mason_null_ls.setup({
-                automatic_installation = true,
-                ensure_installed = {
-                    "stylua",
-                },
-            })
-        end,
-    },
 }
