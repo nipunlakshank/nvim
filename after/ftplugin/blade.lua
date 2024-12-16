f1d1d019f53a50454cd301858de9613c8ed7ce33
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-12-17 04:53:29 +0530

    fix: error when setting commentstring for blade files

--- after/ftplugin/blade.lua
+++ after/ftplugin/blade.lua
@@ -2,11 +2,10 @@ local syntax_group = vim.api.nvim_create_augroup("SyntaxHighlighting", {})
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
     group = syntax_group,
-    pattern = { "*.blade.php" },
+    pattern = "*.blade.php",
     callback = function()
         local comment_ft = require("Comment.ft")
-
-        comment_ft.set("blade", { "//%s", "/*%s*/" })
-        vim.bo.commentstring = comment_ft.get("blade")
+        local commentstring = { "//%s", "/*%s*/" }
+        comment_ft.set("blade", commentstring)
     end,
 })
