0d402e95f57b415482084b8cc1304e2b5e5d05e2
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-12-17 20:05:42 +0530

    refactor: remove unnecessary plugins

--- lua/nipunlakshank/plugins/init.lua
+++ lua/nipunlakshank/plugins/init.lua
@@ -6,12 +6,13 @@ return {
             library = {
                 -- See the configuration section for more details
                 -- Load luvit types when the `vim.uv` word is found
-                { path = "luvit-meta/library", words = { "vim%.uv", "vim%.loop" } },
+                { path = "${3rd}/luv/library", words = { "vim%.uv", "vim%.loop" } },
                 { path = "LazyVim", words = { "LazyVim" } },
                 { path = "wezterm-types", mods = { "wezterm" } },
             },
             -- always enable unless `vim.g.lazydev_enabled = false`
             -- This is the default
+            ---@diagnostic disable-next-line: unused-local
             enabled = function(root_dir)
                 return vim.g.lazydev_enabled == nil and true or vim.g.lazydev_enabled
             end,
@@ -22,7 +23,6 @@ return {
             -- end,
         },
     },
-    { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
     { -- optional cmp completion source for require statements and module annotations
         "hrsh7th/nvim-cmp",
         opts = function(_, opts)
@@ -33,21 +33,4 @@ return {
             })
         end,
     },
-    { -- optional blink completion source for require statements and module annotations
-        "saghen/blink.cmp",
-        opts = {
-            sources = {
-                -- add lazydev to your completion providers
-                completion = {
-                    enabled_providers = { "lsp", "path", "snippets", "buffer", "lazydev" },
-                },
-                providers = {
-                    -- dont show LuaLS require statements when lazydev has items
-                    lsp = { fallback_for = { "lazydev" } },
-                    lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
-                },
-            },
-        },
-    },
-    -- { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
 }
