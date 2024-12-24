7e03df44c3d07bc3b0db43031eb8085d7a8cc23e
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-12-24 16:13:04 +0530

    override vim.ui.input with Snacks.input.input()

--- lua/nipunlakshank/plugins/snacks.lua
+++ lua/nipunlakshank/plugins/snacks.lua
@@ -55,6 +55,7 @@ return {
                     Snacks.debug.backtrace()
                 end
                 vim.print = _G.dd -- Override print to use snacks for `:=` command
+                vim.ui.input = Snacks.input.input
 
                 -- Create some toggle mappings
                 -- Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
