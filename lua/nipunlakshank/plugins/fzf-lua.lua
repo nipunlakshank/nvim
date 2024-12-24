bee1f8462caad3574fcbd09f6375f464b50a77ac
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-12-24 16:12:18 +0530

    register fzf-lua's select ui as vim.select

--- lua/nipunlakshank/plugins/fzf-lua.lua
+++ lua/nipunlakshank/plugins/fzf-lua.lua
@@ -1,14 +1,15 @@
 return {
     "ibhagwan/fzf-lua",
-    event = { "VimEnter" },
+    event = { "VeryLazy" },
     -- optional for icon support
     dependencies = {
         "nvim-tree/nvim-web-devicons",
         "nvim-treesitter/nvim-treesitter"
     },
     config = function()
+        local fzflua = require("fzf-lua")
         -- calling `setup` is optional for customization
-        require("fzf-lua").setup({
+        fzflua.setup({
             "telescope",
             hls = {
                 border = "FloatBorder",
@@ -19,5 +20,7 @@ return {
         local config = require("fzf-lua.config")
         local actions = require("trouble.sources.fzf").actions
         config.defaults.actions.files["ctrl-t"] = actions.open
+
+        require("fzf-lua.providers.ui_select").register({ winopts = { height = 0.33, width = 0.40 } })
     end,
 }
