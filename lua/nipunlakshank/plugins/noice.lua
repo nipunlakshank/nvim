31313209bc3b45f8c1cb8c9ccf1f7ee4874d6a56
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-12-24 19:07:45 +0530

    feat(plugin): add noice.nvim

---
+++ lua/nipunlakshank/plugins/noice.lua
@@ -0,0 +1,75 @@
+return {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+        "MunifTanjim/nui.nvim",
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view. If not available, we use `mini` as the fallback
+        -- "rcarriga/nvim-notify",
+    },
+    config = function()
+        require("noice").setup({
+            notify = { enabled = false },
+            lsp = {
+                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                    ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                },
+                progress = {
+                    enabled = false,
+                    -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
+                    -- See the section on formatting for more details on how to customize.
+                    --- @type NoiceFormat|string
+                    format = "lsp_progress",
+                    --- @type NoiceFormat|string
+                    format_done = "lsp_progress_done",
+                    throttle = 1000 / 30, -- frequency to update lsp progress message
+                    view = "mini",
+                },
+            },
+            -- you can enable a preset for easier configuration
+            presets = {
+                bottom_search = true,         -- use a classic bottom cmdline for search
+                command_palette = true,       -- position the cmdline and popupmenu together
+                long_message_to_split = true, -- long messages will be sent to a split
+                inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+                lsp_doc_border = true,        -- add a border to hover docs and signature help
+            },
+            cmdline = {
+                enabled = true,   -- enables the Noice cmdline UI
+                view = "cmdline", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
+                opts = {},        -- global options for the cmdline. See section on views
+                ---@type table<string, CmdlineFormat>
+                format = {
+                    -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
+                    -- view: (default is cmdline view)
+                    -- opts: any options passed to the view
+                    -- icon_hl_group: optional hl_group for the icon
+                    -- title: set to anything or empty string to hide
+                    cmdline = { pattern = "^:", icon = "", lang = "vim" },
+                    search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+                    search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+                    filter = { title = "shell", pattern = "^:%s*!", icon = "$", lang = "bash" },
+                    lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+                    -- help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+                    help = { pattern = "^:%s*he?l?p?%s+", icon = "󰋖" },
+                    input = { view = "cmdline_input", icon = "󰥻 " }, -- Used by input()
+                    -- lua = false, -- to disable a format, set to `false`
+                },
+            },
+            messages = {
+                -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+                -- This is a current Neovim limitation.
+                enabled = true,              -- enables the Noice messages UI
+                view = "mini",               -- default view for messages
+                view_error = "notify",       -- view for errors
+                view_warn = "notify",        -- view for warnings
+                view_history = "messages",   -- view for :messages
+                view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+            },
+        })
+    end
+}
