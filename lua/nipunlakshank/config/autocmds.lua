f546b816f2c0149c415f1a75a7418fb45ebbde41
Nipun Lakshan <nipunlakshankumara@gmail.com>
2025-02-22 02:46:23 +0530

    feat: lsp renaming for files

--- lua/nipunlakshank/config/autocmds.lua
+++ lua/nipunlakshank/config/autocmds.lua
@@ -21,6 +21,17 @@ autocmd("User", {
     end,
 })
 
+-- Lsp-integrated file renaming
+vim.api.nvim_create_autocmd("User", {
+    pattern = "OilActionsPost",
+    callback = function(event)
+        if event.data.actions.type == "move" then
+            ---@module "snacks.rename"
+            Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+        end
+    end,
+})
+
 -- highlight on yank
 autocmd("TextYankPost", {
     group = highlight_yank_group,
--- lua/nipunlakshank/plugins/file-tree.lua
+++ lua/nipunlakshank/plugins/file-tree.lua
@@ -9,7 +9,18 @@ return {
         "MunifTanjim/nui.nvim",
         -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
-    config = function()
+    config = function(_, opts)
+        local function on_move(data)
+            ---@module "snacks.rename"
+            Snacks.rename.on_rename_file(data.source, data.destination)
+        end
+        local events = require("neo-tree.events")
+        opts.event_handlers = opts.event_handlers or {}
+        vim.list_extend(opts.event_handlers, {
+            { event = events.FILE_MOVED,   handler = on_move },
+            { event = events.FILE_RENAMED, handler = on_move },
+        })
+
         require("neo-tree").setup({
             filesystem = {
                 filtered_items = {
