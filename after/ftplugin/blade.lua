c4cbae10f305a5c2c3712b8a9505d4ae1eae49d8
Nipun Lakshan <nipunlakshankumara@gmail.com>
2025-03-05 00:58:40 +0530

    chore: blade comments
    not fixed yet.

--- after/ftplugin/blade.lua
+++ after/ftplugin/blade.lua
@@ -1,15 +1,7 @@
-local ft_group = vim.api.nvim_create_augroup("FiletypeGroup", {})
-
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-    group = ft_group,
-    pattern = "*.blade.php",
-    callback = function()
-        local comment_ft = require("Comment.ft")
-        local commentstring = { "//%s", "/*%s*/" }
-        if comment_ft then
-            comment_ft.set("blade", commentstring)
-        else
-            vim.bo.commentstring = table.concat(commentstring, ",")
-        end
-    end,
-})
+local comment_ft = require("Comment.ft")
+local commentstring = { "{{--%s--}}" }
+if comment_ft then
+    comment_ft.set("blade", commentstring)
+else
+    vim.bo.commentstring = "{{-- %s --}}"
+end
